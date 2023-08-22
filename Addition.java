996a12ff8b8228edebde33c144587447bf3ea837
firdosexworkz <firdose.xworkz@gmail.com>
2023-08-22 10:48:12 +0530

    message

---
+++ Addition.java
@@ -0,0 +1,22 @@
+class Addition{
+public static void main(String[] args){
+	int a=20,b=30,sum;
+	sum=a+b;
+	System.out.println("Sum of these numbers="+sum);
+	int c=50,d=30,difference;
+	difference=c-d;
+	System.out.println("Difference of these numbers="+difference);
+	int e=6,f=8,product;
+	product=e*f;
+	System.out.println("Product of these numbers="+product);
+	int g=45,h=9,dividend;
+	dividend=g/h;
+	System.out.println("Dividend of these numbers="+dividend);
+System.out.println("Sum of two numbers is called Addition");
+System.out.println("Sum of two numbers is called Addition"+170);
+System.out.println("711"+2);
+System.out.println("711+2");
+System.out.println(711+2);
+System.out.println(711+"2");
+}
+}
